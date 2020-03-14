787bba42e39fb729708b0b245283836a854bd0f8
Banialczele <marcinpachole@gmail.com>
2020-03-14 19:35:35 +0100

    checking db

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('process.env.MONGODB_URI/teacher-course-app',{
+const databasestring = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb:127.0.0.1/teacher-course-app';
+mongoose.connect(databasestring,{
 	useNewUrlParser: true,
 	useFindAndModify: false,
 	useUnifiedTopology: true,
