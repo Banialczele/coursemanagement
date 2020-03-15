97d63dd5e5b5ed6848ef8052ae21d4f735df6fe1
Banialczele <marcinpachole@gmail.com>
2020-03-15 13:06:25 +0100

    Version 1.0.0

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 require('./db/mongoose.js');
 const cors = require('cors');
+const path = require('path');
 
 const studentRouter = require('./routers/student');
-const courseRouter = require('./routers/course');           
+const courseRouter = require('./routers/course');
 const teacherRouter = require('./routers/teacher');
 
 const app = express();
@@ -15,6 +16,14 @@ app.use(express.json());
 app.use(studentRouter);
 app.use(teacherRouter);
 app.use(courseRouter);
+
+if(process.env.NODE_ENV === 'production') {
+	app.use(express.static('../../client/build'));
+
+	app.get('*', (req,res) => {
+		res.sendFile(path.resolve(__dirname, '../../client', 'build', 'index.html'));
+	});
+}
 
 app.listen(port,() => {
 	console.log(`Successfully run server. it is running on port ${port}`);
