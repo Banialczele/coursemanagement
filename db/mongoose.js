c0f18c3d94007eb827fe84b0354cf45597f0683e
Banialczele <marcinpachole@gmail.com>
2020-03-15 14:48:04 +0100

    Version 1.0.0

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // when run on herokku server use that link instead mongodb+srv://admin:admin@coursemanagement-vndhb.mongodb.net/test?retryWrites=true&w=majority
-mongoose.connect('mongodb://127.0.0.1:27017/teacher-course-app',{
+mongoose.connect('mongodb+srv://admin:admin@coursemanagement-vndhb.mongodb.net/test?retryWrites=true&w=majority',{
 	useNewUrlParser: true,
 	useFindAndModify: false,
 	useUnifiedTopology: true,
