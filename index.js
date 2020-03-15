f7c7b6da61e2c0c1b4563c81799f4f13cc89cd14
Banialczele <marcinpachole@gmail.com>
2020-03-15 14:02:45 +0100

    restructure app directories

--- index.js
+++ index.js
@@ -6,16 +6,25 @@ const path = require('path');
 const studentRouter = require('./routers/student');
 const courseRouter = require('./routers/course');
 const teacherRouter = require('./routers/teacher');
-
 const app = express();
 app.use(cors());
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5005;
 
 
 app.use(express.json());
 app.use(studentRouter);
 app.use(teacherRouter);
 app.use(courseRouter);
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*',(req,res) => {
+		res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+	});
+}
 
 app.listen(port,() => {
 	console.log(`Successfully run server. it is running on port ${port}`);
