b71aa97311b125162d7e1623e8563227135015f1
Banialczele <marcinpachole@gmail.com>
2020-03-15 13:42:46 +0100

    some changes

--- coursemanagement/tree/master/server/server/src/db/mongoose.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('process.env.MONGODB_URI/teacher-course-app',{
-	useNewUrlParser: true,
-	useFindAndModify: false,
-	useUnifiedTopology: true,
-	useCreateIndex: true
-});
--- server/src/index.js
+++ server/src/index.js
@@ -9,21 +9,13 @@ const teacherRouter = require('./routers/teacher');
 
 const app = express();
 app.use(cors());
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 
 app.use(express.json());
 app.use(studentRouter);
 app.use(teacherRouter);
 app.use(courseRouter);
-
-if(process.env.NODE_ENV === 'production') {
-	app.use(express.static('../../../client/build'));
-
-	app.get('*', (req,res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
-}
 
 app.listen(port,() => {
 	console.log(`Successfully run server. it is running on port ${port}`);
